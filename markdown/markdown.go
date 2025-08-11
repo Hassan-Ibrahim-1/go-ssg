071f93f2d3ac0d7d078e3a8ab69688e3131b7724
Hassan-Ibrahim-1 <hassanibrahimasi@gmail.com>
2025-08-11 17:03:19 -0400

    Fix ToHTML

--- markdown/markdown.go
+++ markdown/markdown.go
@@ -1,6 +1,8 @@
 package markdown
 
 import (
+	"fmt"
+
 	"github.com/gomarkdown/markdown"
 	"github.com/microcosm-cc/bluemonday"
 )
@@ -11,7 +13,16 @@ type HTMLDoc struct {
 }
 
 func ToHTML(md []byte) (HTMLDoc, error) {
-	unsanitized := markdown.ToHTML(md, nil, nil)
-	_ = bluemonday.UGCPolicy().SanitizeBytes(unsanitized)
-	return HTMLDoc{}, nil
+	metadata, content, err := parseMetadata(md)
+	if err != nil {
+		return HTMLDoc{}, fmt.Errorf("Failed to parse metadata: %w", err)
+	}
+
+	unsanitized := markdown.ToHTML(content, nil, nil)
+	html := bluemonday.UGCPolicy().SanitizeBytes(unsanitized)
+
+	return HTMLDoc{
+		Metadata: metadata,
+		Content:  html,
+	}, nil
 }
